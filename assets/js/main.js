a5d8bd8eafba9ba7dbe7a4be03045a55272b5ca4
DevilRaider-Dev <deviltwix3@gmail.com>
2021-08-17 13:48:15 +0200

    final

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+function changeBrutto() {
+    document.getElementById("amountText").innerHTML = "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro";
+    document.getElementById("resultText").innerHTML = "Nettobetrag";
+}
+function changeNetto() {
+    document.getElementById("amountText").innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
+    document.getElementById("resultText").innerHTML = "Bruttobetrag (Endpreis)";
+}
+
+function getPercent() {
+}
+
+function calculateTax() {
+
+    let input = Number(document.getElementById("amount").value);
+    let percent = 0;
+    let taxAmount = 0;
+    let result = 0;
+
+    if (document.getElementById("taxHigh").checked) {
+        percent = .19;
+    } else {
+        percent = .07;
+    }
+
+    if (document.getElementById("netto").checked && input != null) {
+        document.getElementById("resultTaxAmount").innerHTML = input * percent;
+        document.getElementById("resultTax").innerHTML = input * (1 + percent);
+    } else {
+        document.getElementById("resultTaxAmount").innerHTML = input * percent;
+        document.getElementById("resultTax").innerHTML = input / (1 + percent);
+    }
+
+}
